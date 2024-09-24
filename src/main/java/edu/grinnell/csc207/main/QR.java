e4346bdf9b418eaaefb446b3b9e0c176a2681d1c
Zhu <zhuharri@licklider.cs.grinnell.edu>
2024-09-24 15:45:19 -0500

    Add instructions to QR.

--- src/main/java/edu/grinnell/csc207/main/QR.java
+++ src/main/java/edu/grinnell/csc207/main/QR.java
@@ -1,11 +1,11 @@
 package edu.grinnell.csc207.main;
 
-import edu.grinnell.csc207.util.Quadratic;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
+import edu.grinnell.csc207.util.IOUtils;
+import edu.grinnell.csc207.util.Quadratic;
 /**
  * Interactively compute quadratic roots.
  *
@@ -27,7 +27,15 @@ public class QR {
     PrintWriter pen = new PrintWriter(System.out, true);
     BufferedReader eyes = new BufferedReader(new InputStreamReader(System.in));
 
-    // ...
+    pen.println("Hi! I'm here to help you understand quadratic functions.");
+    int a = IOUtils.readInt(pen, eyes, "Please enter a (the coefficient of the squared term): ");
+    int b = IOUtils.readInt(pen, eyes, "Please enter b (the coefficient of the linear term) : ");
+    int c = IOUtils.readInt(pen, eyes, "Please enter c (the constant)                       : ");
+
+    Quadratic quad = new Quadratic(a, b, c);
+    Double result = quad.smallerRoot();
+    pen.println("The smaller root of " + quad + " = " + result);
+    pen.printf("Experimentally, %d*%d^2 + %d*%d + %d = %d\n", a, result.intValue(), b, result.intValue(), c, quad.evaluate(result.intValue()));
 
     eyes.close();
     pen.close();
